0a16ec17e22229f6361392662d24cce58323fda8
Chris Hamilton <chris_h_27@hotmail.ca>
2022-07-07 03:11:26 -0400

    add saveOnChange directive

--- src/app/save-on-change.directive.ts
+++ src/app/save-on-change.directive.ts
@@ -1,15 +1,15 @@
-import { Directive, Input, SimpleChanges } from '@angular/core';
+import { Directive, HostListener, Input, SimpleChanges } from '@angular/core';
 import { ProjectService } from './project.service';
 
 @Directive({
   selector: '[saveOnChange]',
 })
 export class SaveOnChangeDirective {
-  @Input() saveOnChange: any;
-
   constructor(private projectService: ProjectService) {}
 
-  ngOnChanges() {
-    this.projectService.save();
+  @HostListener('ngModelChange', [])
+  onChange() {
+    // Queued so it saves AFTER the model is updated
+    queueMicrotask(() => this.projectService.save());
   }
 }
