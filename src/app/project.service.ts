bf34b97d48bd779364bb68b0646415707cfbb5bf
Chris Hamilton <chris_h_27@hotmail.ca>
2022-07-07 02:41:59 -0400

    implement project service

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
 
+export class Project {
+  name = 'New Project';
+  tasks: ProjectTask[] = [];
+  newTask() {
+    this.tasks.push(new ProjectTask());
+  }
+}
+
+export class ProjectTask {
+  completed = false;
+  name = 'New Task';
+  description = '';
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProjectService {
+  private dataKey = 'PROJECTS';
+  private currentIndex?: number;
+  projects: Project[] = [];
 
-  constructor() { }
+  constructor() {
+    this.load();
+  }
+
+  get currentProject(): Project | undefined {
+    return this.currentIndex !== undefined
+      ? this.projects[this.currentIndex]
+      : undefined;
+  }
+
+  newProject() {
+    this.projects.push(new Project());
+    this.currentIndex = this.projects.length - 1;
+    this.save();
+  }
+
+  switchTo(index: number) {
+    this.currentIndex = index;
+  }
+
+  deleteCurrentProject() {
+    if (this.currentIndex === undefined) return;
+    this.projects.splice(this.currentIndex, 1);
+    this.currentIndex = undefined;
+    this.save();
+  }
+
+  load() {
+    const data = localStorage.getItem(this.dataKey);
+    if (data === null) this.projects = [];
+    else this.projects = JSON.parse(data);
+  }
+
+  save() {
+    localStorage.setItem(this.dataKey, JSON.stringify(this.projects));
+  }
 }
